86521c767ab0fc32c57247796a9991d89c6e0446
gabriel <gabriel.becker.59@gmail.com>
2019-06-24 18:24:02 -0300

    rename fucn

--- src/BatattiColoratti/BatattiColoratti.ino
+++ src/BatattiColoratti/BatattiColoratti.ino
@@ -46,3 +46,29 @@ void setup()
 void loop()
 {
 }
+
+
+void initTimeoutInterrupt()
+{
+  
+  noInterrupts();           // disable all interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+
+  // Set timer1_counter to the correct value for our interrupt interval
+  //timer1_counter = 64911;   // preload timer 65536-16MHz/256/100Hz
+  //timer1_counter = 64286;   // preload timer 65536-16MHz/256/50Hz
+  timer1_counter = 60*34286;   // preload timer 65536-16MHz/256/2Hz
+  
+  TCNT1 = timer1_counter;   // preload timer
+  TCCR1B |= (1 << CS12);    // 256 prescaler 
+  TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
+  interrupts();             // enable all interrupts
+}
+
+ISR(TIMER1_OVF_vect)        // interrupt service routine 
+{
+  TCNT1 = timer1_counter;   // preload timer
+  para();
+  
+}
